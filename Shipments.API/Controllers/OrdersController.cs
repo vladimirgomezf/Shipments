29b73f0193a05c32277ec51700d60a35fae1fc67
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-30 22:38:41 -0800

    Add Orders and Shipments Controllers

---
+++ Shipments.API/Controllers/OrdersController.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shipments.API.Models;
+using Shipments.API.Data;
+
+namespace Shipments.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : Controller
+    {
+        private readonly DataContext _context;
+
+        public OrdersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Orders>>> Get()
+        {
+            return Ok(await _context.Orders.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Orders>> Get(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return BadRequest("Order Not found!");
+            }
+            return Ok(order);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Orders>>> AddOrder([FromBody] Orders order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return Ok(_context.Orders.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Orders>>> UpdateOrder(Orders request)
+        {
+            var dbOrder = await _context.Orders.FindAsync(request.Id);
+            if (dbOrder == null)
+                return BadRequest("Order not found!");
+
+            dbOrder.Customer_Id = request.Customer_Id;
+            dbOrder.Date = request.Date;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Orders.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Orders>>> DeleteOrder(int id)
+        {
+            var dbOrder = await _context.Orders.FindAsync(id);
+            if (dbOrder == null)
+                return BadRequest("Order not found!");
+
+            _context.Orders.Remove(dbOrder);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Orders.ToListAsync());
+        }
+    }
+}
---
+++ Shipments.API/Controllers/ShipmentsController.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Shipments.API.Models;
+using Shipments.API.Data;
+
+namespace Shipments.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShipmentsController : Controller
+    {
+        private readonly DataContext _context;
+
+        public ShipmentsController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Shipments.API.Models.Shipments>>> Get()
+        {
+            return Ok(await _context.Shipments.ToListAsync());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Shipments.API.Models.Shipments>> Get(int id)
+        {
+            var ships = await _context.Shipments.FindAsync(id);
+            if (ships == null)
+            {
+                return BadRequest("Shipment Not found!");
+            }
+            return Ok(ships);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<Shipments.API.Models.Shipments>>> AddOrder([FromBody] Shipments.API.Models.Shipments shipment)
+        {
+            _context.Shipments.Add(shipment);
+            await _context.SaveChangesAsync();
+
+            return Ok(_context.Shipments.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<Shipments.API.Models.Shipments>>> UpdateOrder(Shipments.API.Models.Shipments request)
+        {
+            var dbShips = await _context.Shipments.FindAsync(request.Id);
+            if (dbShips == null)
+                return BadRequest("Shipment not found!");
+
+            dbShips.Order_Id = request.Order_Id;
+            dbShips.Date_Start = request.Date_Start;
+            dbShips.Date_End = request.Date_End;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Shipments.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Shipments.API.Models.Shipments>>> DeleteOrder(int id)
+        {
+            var dbShips = await _context.Shipments.FindAsync(id);
+            if (dbShips == null)
+                return BadRequest("Shipment not found!");
+
+            _context.Shipments.Remove(dbShips);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Shipments.ToListAsync());
+        }
+    }
+}
