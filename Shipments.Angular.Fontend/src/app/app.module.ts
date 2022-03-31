e32d980b75805c79026597a559265f81edcf9935
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-31 00:57:25 -0800

    remove the controllers initialization

--- Shipments.Angular.Fontend/src/app/app.module.ts
+++ Shipments.Angular.Fontend/src/app/app.module.ts
@@ -3,22 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CustomersControllerComponent } from './customers-controller/customers-controller.component';
-import { OrdersControllerComponent } from './orders-controller/orders-controller.component';
-import { ShipmentsControllerComponent } from './shipments-controller/shipments-controller.component';
-
 @NgModule({
-  declarations: [
-    AppComponent,
-    CustomersControllerComponent,
-    OrdersControllerComponent,
-    ShipmentsControllerComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
