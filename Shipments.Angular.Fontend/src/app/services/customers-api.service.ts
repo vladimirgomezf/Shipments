6ac627b9a46d68c2eab2332edde5f8d44c73a9a8
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-31 12:55:08 -0800

    Add the basic CRUD for customers service

--- Shipments.Angular.Fontend/src/app/services/customers-api.service.ts
+++ Shipments.Angular.Fontend/src/app/services/customers-api.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CustomersApiService {
+  http: HttpClient;
 
-  constructor() { }
+  constructor(http: HttpClient) {
+    this.http = http;
+  }
+
+  getCustomersList(): Observable<any[]> {
+    return this.http.get<any>(
+      `${environment.originUrl}${environment.apiUrl}/customers`
+    );
+  }
+
+  addCustomer(data: any) {
+    return this.http.post(
+      `${environment.originUrl}${environment.apiUrl}/customers`,
+      data
+    );
+  }
+
+  updateCustomer(id: number | string, data: any) {
+    return this.http.put(
+      `${environment.originUrl}${environment.apiUrl}/customers/${id}`,
+      data
+    );
+  }
+
+  deleteCustomer(id: number | string) {
+    return this.http.delete(
+      `${environment.originUrl}${environment.apiUrl}/customers/${id}`,
+      data
+    );
+  }
 }
