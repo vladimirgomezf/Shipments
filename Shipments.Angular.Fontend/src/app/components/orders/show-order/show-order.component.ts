09048e81df0b20680b7cc9f4830f97d06af8c880
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-31 23:21:27 -0800

    Add the logic to show the orders in a datatable

--- Shipments.Angular.Fontend/src/app/components/orders/show-order/show-order.component.ts
+++ Shipments.Angular.Fontend/src/app/components/orders/show-order/show-order.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { OrdersApiService } from 'src/app/services/orders-api.service';
 
 @Component({
   selector: 'app-show-order',
   templateUrl: './show-order.component.html',
-  styleUrls: ['./show-order.component.css']
+  styleUrls: ['./show-order.component.css'],
 })
 export class ShowOrderComponent implements OnInit {
+  orderList$!: Observable<any[]>;
 
-  constructor() { }
+  constructor(private service: OrdersApiService) {}
 
   ngOnInit(): void {
+    this.orderList$ = this.service.getOrdersList();
   }
-
 }
