a47b4de1cc45665214a858d43bad953684b4034a
Vladimir Gómez Fumero <vladimirg.fumero@gmail.com>
2022-03-31 23:19:52 -0800

    Add the logic to show the shipments in a datatable

--- Shipments.Angular.Fontend/src/app/components/shipments/show-shipment/show-shipment.component.ts
+++ Shipments.Angular.Fontend/src/app/components/shipments/show-shipment/show-shipment.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShipmentsApiService } from 'src/app/services/shipments-api.service';
 
 @Component({
   selector: 'app-show-shipment',
   templateUrl: './show-shipment.component.html',
-  styleUrls: ['./show-shipment.component.css']
+  styleUrls: ['./show-shipment.component.css'],
 })
 export class ShowShipmentComponent implements OnInit {
+  shipmentList$!: Observable<any[]>;
 
-  constructor() { }
+  constructor(private service: ShipmentsApiService) {}
 
   ngOnInit(): void {
+    this.shipmentList$ = this.service.getShipmentsList();
   }
-
 }
